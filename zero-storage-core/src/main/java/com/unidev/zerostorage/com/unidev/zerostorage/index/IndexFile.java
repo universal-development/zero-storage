4096d7dbc16c08dc38a2bbd3cbe6e4d16c605573
Denis O <denis@universal-development.com>
2016-07-24 15:40:48 +0300

    Storage file index

---
+++ zero-storage-core/src/main/java/com/unidev/zerostorage/com/unidev/zerostorage/index/IndexFile.java
@@ -0,0 +1,29 @@
+package com.unidev.zerostorage.com.unidev.zerostorage.index;
+
+/**
+ * Index file name
+ */
+public class IndexFile {
+
+    private String file;
+
+    public IndexFile() {
+
+    }
+
+    public IndexFile(String file) {
+        this.file = file;
+    }
+
+    public String file() {
+        return this.file;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+}
