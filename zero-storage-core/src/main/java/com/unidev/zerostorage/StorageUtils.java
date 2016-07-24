1108115c434d17b663b821e9d9f730bc148022ae
Denis O <denis@universal-development.com>
2016-07-24 10:47:54 +0300

    Storage utils class

---
+++ zero-storage-core/src/main/java/com/unidev/zerostorage/StorageUtils.java
@@ -0,0 +1,21 @@
+package com.unidev.zerostorage;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/**
+ * Storage util classes
+ */
+public class StorageUtils {
+
+    public static ObjectMapper STORAGE_OBJECT_MAPPER = new ObjectMapper() {{
+        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    }};
+
+    static {
+
+    }
+
+}
