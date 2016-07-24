1a81905b4e37914fa9723d39078506a3e35c0d7d
Denis O <denis@universal-development.com>
2016-07-24 16:53:02 +0300

    Storage index class

--- zero-storage-core/src/main/java/com/unidev/zerostorage/com/unidev/zerostorage/index/IndexStorage.java
+++ zero-storage-core/src/main/java/com/unidev/zerostorage/com/unidev/zerostorage/index/IndexStorage.java
@@ -1,29 +1,64 @@
 package com.unidev.zerostorage.com.unidev.zerostorage.index;
 
+import com.unidev.zerostorage.Metadata;
+import com.unidev.zerostorage.Storage;
+
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
+
+import static com.unidev.zerostorage.StorageMapper.storageMapper;
 
 /**
  * Storage index file
  */
 public class IndexStorage {
+    public static final String INDEX_FILE = "index.json";
+    public static final String FILE_EXT = ".json";
+    public static final int FIRST_INDEX = 1;
 
     private File storageRoot;
-    private List<String> storageFiles;
+    private File indexFile;
+    private Storage index;
 
     public IndexStorage() {
-        storageFiles = new ArrayList<>();
+    }
+
+    public IndexStorage load() {
+        if (indexFile.exists()) {
+            index = storageMapper().loadSource(indexFile).load();
+        } else {
+            index = new Storage();
+        }
+        return this;
+    }
+
+    public void save() {
+        storageMapper().saveSource(indexFile).save(index);
     }
 
     public IndexStorage storageRoot(File file) {
         this.storageRoot = file;
+        this.indexFile = new File(storageRoot, INDEX_FILE);
         return this;
     }
 
+    public List<Metadata> sorageFiles() {
+        return index.metadata();
+    }
 
-    public void loadIndex() {
+    public IndexStorage addMetadata(Metadata metadata) {
+        if (metadata.isEmpty()) {
+            Storage storage = new Storage();
+            File metadataFile = new File(storageRoot, FIRST_INDEX + FILE_EXT);
+            storageMapper().saveSource(metadataFile).save(storage);
 
+            Metadata indexMetadata = new Metadata();
+            indexMetadata.setLink(metadataFile.getName());
+            indexMetadata.set_id(metadataFile.getName());
+            index.addMetadateFirst(indexMetadata);
+            save();
+        }
+        return this;
     }
 
 }
