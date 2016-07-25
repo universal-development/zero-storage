0b7d78fc0f863c00ea174aaa2fb2b5ca10567eec
Denis <denis@universal-development.com>
2016-07-25 18:04:40 +0300

    Test cases for saving index with blank meta and test for saving index with mata

--- zero-storage-core/src/test/java/com/unidev/zerostorage/TestIndexStorage.java
+++ zero-storage-core/src/test/java/com/unidev/zerostorage/TestIndexStorage.java
@@ -16,11 +16,27 @@ public class TestIndexStorage {
     public TemporaryFolder folder= new TemporaryFolder();
 
     @Test
-    public void testIndexStorageSave() {
+    public void testBlankIndexStorageSave() {
         IndexStorage indexStorage = new IndexStorage();
 
         Storage blankIndex = new Storage();
         indexStorage.index(blankIndex);
+
+        File root = folder.getRoot();
+
+        indexStorage.storageRoot(root);
+        indexStorage.save();
+
+    }
+
+    @Test
+    public void testIndexStorageSave() {
+        IndexStorage indexStorage = new IndexStorage();
+
+        Storage index = new Storage();
+        index.details().put("Potato", "Tomato");
+        index.details().put("Tomato", null);
+        indexStorage.index(index);
 
         File root = folder.getRoot();
 
