bd3a1e70901ce02e334121909b6e04d08feb2b3e
Denis O <denis@universal-development.com>
2016-07-31 23:13:33 +0300

    Android methods for loading storage

---
+++ zulu-storage-android-core/src/main/java/com/unidev/zulustorage/AssetIndexLoader.java
@@ -0,0 +1,7 @@
+package com.unidev.zulustorage;
+
+/**
+ * Created by binary256 on 7/31/16.
+ */
+public class AssetIndexLoader {
+}
---
+++ zulu-storage-android-core/src/main/java/com/unidev/zulustorage/AssetStorageLoader.java
@@ -0,0 +1,17 @@
+package com.unidev.zulustorage;
+
+/**
+ * Storage loader from asset files
+ */
+public class AssetStorageLoader {
+
+    /**
+     * Load
+     * @param path
+     * @return
+     */
+    public static Storage load(String path) {
+
+    }
+
+}
