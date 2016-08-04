6f8167387a8cc51bd2a31ff74b3d4ea4654c80a5
Denis <denis@universal-development.com>
2016-08-04 16:54:50 +0300

    Storage loading from android assets

--- zulu-storage-android-core/src/main/java/com/unidev/zulustorage/AssetStorageLoader.java
+++ zulu-storage-android-core/src/main/java/com/unidev/zulustorage/AssetStorageLoader.java
@@ -1,5 +1,15 @@
 package com.unidev.zulustorage;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.unidev.zulustorage.StorageMapper.*;
+
+
 /**
  * Storage loader from asset files
  */
@@ -10,8 +20,14 @@ public class AssetStorageLoader {
      * @param path
      * @return
      */
-    public static Storage load(String path) {
-
+    public static Storage load(Context context, String path) {
+        AssetManager assets = context.getAssets();
+        try (InputStream inputStream = assets.open(path)){
+            return storageMapper().loadSource(inputStream).load();
+        } catch (IOException e) {
+            Log.e("AssetStorageLoader", "Failed to load storage from " + path);
+            e.printStackTrace();
+        }
     }
 
 }
